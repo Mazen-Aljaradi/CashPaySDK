4dc79425401ee3536c5bc3db9c8a3ecb201164c0
mazen <m.f.aljaradi@gmail.com>
2021-10-24 00:27:09 +0300

    v3

--- CashPaySDK/src/main/java/com/tamkeen/cashpaysdk/CashPay_Button.java
+++ CashPaySDK/src/main/java/com/tamkeen/cashpaysdk/CashPay_Button.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -17,15 +18,17 @@ import androidx.annotation.Nullable;
 public class CashPay_Button extends ImageButton {
     public CashPay_Button(final Context context) {
         super(context);
-        Drawable image=(Drawable)getResources().getDrawable(R.drawable.shap_cash);
-setImageDrawable(image);
-setBackgroundColor(Color.parseColor("#006666"));
+
        // setImageDrawable(R.drawable.shap_cash);
 
     }
 
-
-
+    public CashPay_Button(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        Drawable image=(Drawable)getResources().getDrawable(R.drawable.shap_cash);
+        setImageDrawable(image);
+        setBackgroundColor(Color.parseColor("#006666"));
+    }
 
     public  void  Runner(Activity activity){
 
