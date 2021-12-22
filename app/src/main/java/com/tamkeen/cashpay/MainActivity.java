25c44f1d48e8598305167e2b6957ac27a31c6615
mazen <m.f.aljaradi@gmail.com>
2021-12-23 01:05:53 +0300

    v3

--- app/src/main/java/com/tamkeen/cashpay/MainActivity.java
+++ app/src/main/java/com/tamkeen/cashpay/MainActivity.java
@@ -1,8 +1,15 @@
 package com.tamkeen.cashpay;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+
+import com.tamkeen.cashpaysdk.CashPay;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
